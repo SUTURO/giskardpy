f4b675107d218fe3a72325ee54fbc3570e9d6d57
JuliSlz <juschulz@uni-bremen.de>
2025-04-24 12:39:12 +0200

    robocup adjustments

--- giskardpy/motion_graph/monitors/force_torque_monitor.py
+++ giskardpy/motion_graph/monitors/force_torque_monitor.py
@@ -273,7 +273,7 @@ class ShelfGraspThresholdStrategy(ThresholdStrategy):
     reference_frame = 'hand_gripper_tool_frame'
 
     def check_thresholds(self, rob_force, rob_torque):
-        force_z_threshold = 30
+        force_z_threshold = 40
 
         if abs(rob_force[2]) >= force_z_threshold:
             get_middleware().loginfo(
